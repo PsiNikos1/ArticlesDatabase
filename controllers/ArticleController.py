a146a8318998e55225052d0c957a199bdf92d145
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-06 21:33:49 +0300

    Add basic CRUD utility

---
+++ controllers/ArticleController.py
@@ -0,0 +1,25 @@
+class ArticleController:
+
+    def get_all_articles(self):
+        """
+        Get all articles by any user
+        :return:
+        """
+
+    def filter_articles(self) -> list:
+        """
+        User can filter that list by year, month, author(s), tag(s) and keywords
+        :return: A list of Articles
+        """
+
+    def update_article(self):
+        """
+        User can update their own articles
+        :return:
+        """
+
+    def delete_article(self):
+        """
+        User can delete their own articles
+        :return:
+        """
---
+++ controllers/CommentController.py
@@ -0,0 +1,31 @@
+class CommentController:
+
+    def create_comment(self):
+        """
+        User can Create comments on any article
+        :return:
+        """
+
+    def update_comment(self):
+        """
+        User can Update comments on any article
+        :return:
+        """
+
+    def delete_comment(self):
+        """
+        User can Delete comments on any article
+        :return:
+        """
+
+    def get_comment(self):
+        """
+        User can Get comments on any article
+        :return:
+        """
+
+    def get_all_comments(self):
+        """
+        Get all comments on a specific article
+        :return:
+        """
