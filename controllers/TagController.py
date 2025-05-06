d3ba352565240939789a201be83f4d1d566d5cda
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-06 21:56:44 +0300

    add controller for Tag

---
+++ controllers/TagController.py
@@ -0,0 +1,21 @@
+
+class TagController:
+
+    def create_tag(self):"""
+        Create a tag on an article
+        :return: 
+        """
+
+
+    def get_all_tags(self):
+        """
+        Get all tags on a specific article
+        :return:
+        """
+
+    def delete_tag(self, tag_id):
+       """
+        Delete a tag from the article
+       :param tag_id:
+       :return:
+       """
