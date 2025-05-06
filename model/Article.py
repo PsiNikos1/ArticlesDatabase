ddecedf6051310fc0528dd290bbe418941cc3ff3
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-06 21:06:09 +0300

    Add basic model

---
+++ model/Article.py
@@ -0,0 +1,9 @@
+class Article:
+
+    def __init__(self):
+        self.identifier = ""
+        self.publication_date=""
+        self.authors=[]
+        self.abstract=""
+        self.title=""
+        self.tags=[]
