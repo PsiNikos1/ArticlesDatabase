966a107322893dff7d4363884bef02b6b484563e
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-08 01:46:23 +0300

    Implement basic CRUD endpoints for comment

--- controllers/CommentController.py
+++ controllers/CommentController.py
@@ -1,3 +1,10 @@
+from flask import jsonify, request
+from jinja2.runtime import new_context
+
+from initializer._init_ import db
+from model.Comment import Comment
+
+
 class CommentController:
 
     def create_comment(self):
@@ -11,21 +18,38 @@ class CommentController:
         User can Update comments on any article
         :return:
         """
+        data = request.json
+        comment = Comment.query.get_or_404(data["id"])
+        new_content = data["content"]
+        if not new_content:
+            return jsonify({"error": "Missing 'content' field"}), 400
+        comment.content = new_content
+        db.session.commit()
+        return jsonify({"message": "Comment updated"}), 204
 
-    def delete_comment(self):
+
+    def delete_comment(self, comment_id):
         """
         User can Delete comments on any article
         :return:
         """
+        comment = Comment.query.get_or_404(comment_id)
+        db.session.delete(comment)
+        db.session.commit()
+        return jsonify({"message": f"Author with id {comment.id} deleted"})
 
-    def get_comment(self):
+    def get_comments(self, article_id):
         """
         User can Get comments on any article
         :return:
         """
+        comments = Comment.query.filter_by(article_id=article_id).all()
+        return jsonify([comment.to_dict() for comment in comments])
 
     def get_all_comments(self):
         """
         Get all comments on a specific article
         :return:
         """
+        comments = Comment.query.all()
+        return jsonify([comment.to_dict() for comment in comments])
--- routers/CommentRoutes.py
+++ routers/CommentRoutes.py
@@ -2,8 +2,11 @@ from flask import Blueprint
 
 from controllers.CommentController import CommentController
 
-comment_blueprint = Blueprint('comments', __name__, url_prefix='/comment')
+comment_blueprint = Blueprint('comments', __name__, url_prefix='/comments')
 
 controller = CommentController()
 
-# comment_blueprint.add_url_rule('/getAll', methods=['GET'], view_func=controller.get_all_articles)
+comment_blueprint.add_url_rule('/getComments/<int:article_id>', methods=['GET'], view_func=controller.get_comments)
+comment_blueprint.add_url_rule('/delete/<int:comment_id>', methods=['DELETE'], view_func=controller.delete_comment)
+comment_blueprint.add_url_rule('/getAll', methods=['GET'], view_func=controller.get_all_comments)
+comment_blueprint.add_url_rule('/update', methods=['PUT'], view_func=controller.update_comment)
