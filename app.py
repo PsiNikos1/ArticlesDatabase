224f60b5aa93b7b21fe74841cc03c5a21e6295f5
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-07 23:54:32 +0300

    add the start point

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from initializer import populateTables
+from initializer._init_ import init_db, db
+from routers.ArticleRoutes import article_blueprint
+from routers.AuthorRoutes import author_blueprint
+from routers.CommentRoutes import comment_blueprint
+
+
+app = Flask(__name__)
+init_db(app)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    populateTables.populate_authors()
+    populateTables.populate_articles()
+
+app.register_blueprint(article_blueprint)
+app.register_blueprint(comment_blueprint)
+app.register_blueprint(author_blueprint)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
