5c20f44b3d39635a78547e5dbe40dda759d82e33
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-08 00:40:33 +0300

    Implement basic CRUD endpoints for Author

--- controllers/AuthorController.py
+++ controllers/AuthorController.py
@@ -1,5 +1,6 @@
-from flask import jsonify
+from flask import jsonify, request
 
+from initializer._init_ import db
 from model.Author import Author
 
 
@@ -8,3 +9,33 @@ class AuthorController:
     def getAllAuthors(self):
         authors = Author.query.all()
         return jsonify([author.to_dict() for author in authors])
+
+    def create_author(self):
+        data = request.json
+
+        new_author = Author(name=data["name"])
+        db.session.add(new_author)
+        db.session.commit()
+
+        return jsonify({"message": "Author created", "id": new_author.id}), 201
+
+    def update_author(self):
+        data = request.json
+        author = Author.query.get_or_404(data["id"])
+
+        new_name = data.get("name")
+        if not new_name:
+            return jsonify({"error": "Missing 'name' field"}), 400
+
+        author.name = new_name
+        db.session.commit()
+
+        return jsonify({"message": "Author updated"}), 204
+
+    def delete_author(self, author_id):
+        author = Author.query.get_or_404(author_id)
+        db.session.delete(author)
+        db.session.commit()
+        return jsonify({"message": f"Author with id {author.id} deleted"})
+
+
--- routers/AuthorRoutes.py
+++ routers/AuthorRoutes.py
@@ -2,8 +2,12 @@ from flask import Blueprint
 
 from controllers.AuthorController import AuthorController
 
-author_blueprint = Blueprint('authors', __name__, url_prefix='/author')
+author_blueprint = Blueprint('authors', __name__, url_prefix='/authors')
 
 controller = AuthorController()
 
 author_blueprint.add_url_rule('/getAll', methods=['GET'], view_func=controller.getAllAuthors)
+author_blueprint.add_url_rule('/create', methods=['POST'], view_func=controller.create_author)
+author_blueprint.add_url_rule('/update', methods=['PUT'], view_func=controller.update_author)
+author_blueprint.add_url_rule('/delete/<int:author_id>', methods=['DELETE'], view_func=controller.delete_author)
+
