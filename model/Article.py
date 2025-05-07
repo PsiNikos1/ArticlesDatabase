a7582b5756237f31472651e7854003700bc0be43
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-07 23:52:20 +0300

    add authors to the printout of article

--- model/Article.py
+++ model/Article.py
@@ -1,5 +1,6 @@
 from initializer.TablesRelations import article_authors, article_tags
 from initializer._init_ import db
+from model.Comment import Comment
 
 
 class Article(db.Model):
@@ -10,7 +11,6 @@ class Article(db.Model):
     abstract = db.Column(db.Text)
     publication_date = db.Column(db.Date)
 
-    # #Set relationships
     authors = db.relationship('Author', secondary=article_authors, backref='articles')
     tags = db.relationship('Tag', secondary=article_tags, backref='articles')
     comments = db.relationship('Comment', backref='article', cascade="all, delete-orphan")
@@ -19,10 +19,11 @@ class Article(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
-            "identifier": self.name,
-            "title": self.name,
-            "abstract": self.name,
-            "publication_date": self.name
+            "identifier": self.identifier,
+            "title": self.title,
+            "abstract": self.abstract,
+            "publication_date": self.publication_date,
+            "authors": [author.to_dict() for author in self.authors]
         }
 
 
