ed58a4d9607a4e40cd5bfa6343031cf7190e7e36
psilodimitrakopoulos <psilodimitrakopoulos@outlook.com>
2025-05-09 18:22:42 +0300

    View must be paginated, with a maximum of 100 rows per page

--- routers/ArticleRoutes.py
+++ routers/ArticleRoutes.py
@@ -6,9 +6,9 @@ article_blueprint = Blueprint('articles', __name__, url_prefix='/articles')
 
 controller = ArticleController()
 
-article_blueprint.add_url_rule('/getAll', methods=['GET'], view_func=controller.get_all_articles)
+article_blueprint.add_url_rule('/getAll/page/<int:page_number>', methods=['GET'], view_func=controller.get_all_articles)
 article_blueprint.add_url_rule('/create', methods=['POST'], view_func=controller.createArticle)
 article_blueprint.add_url_rule('/update', methods=['PUT'], view_func=controller.update_article)
-article_blueprint.add_url_rule('/delete/', methods=['DELETE'], view_func=controller.delete_article)
-article_blueprint.add_url_rule('/filter', methods=['POST'], view_func=controller.filter_article)
+article_blueprint.add_url_rule('/delete', methods=['DELETE'], view_func=controller.delete_article)
+article_blueprint.add_url_rule('/filter', methods=['POST'], view_func=controller.filter_article )
 article_blueprint.add_url_rule('/downloadcsv', methods=['POST'], view_func=controller.download_filtered_articles_csv)
